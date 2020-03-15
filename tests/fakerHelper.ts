4c669cffb324cb4e489799f9376e7a7456c21156
Khai Nguyen <khaindk@gmail.com>
2020-03-15 16:18:40 +0700

    generate json seed data

--- tests/fakerHelper.ts
+++ tests/fakerHelper.ts
@@ -1,19 +1,22 @@
 var faker = require('faker');
 
-const generateUser = () => {
+const generateUser = (id: number) => {
     return {
+        id: id,
         userName: faker.internet.userName(),
         password: faker.internet.password(),
         email: faker.internet.email()
     }
 }
 
-const generateIdiom = () => {
+const generateIdiom = (id: number) => {
     return {
+      id: id,
       idiom: faker.lorem.words(),
       meaning: faker.lorem.sentence(),
       origin: faker.lorem.paragraph(),
-      example: faker.lorem.paragraph()
+      example: faker.lorem.paragraph(),
+      authorId: 1 // admin
       // TODO comments
     }
 }
